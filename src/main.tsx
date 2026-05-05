effc03cac2aeffe44706bae4cabde79da07a6be0
Deywd Alexadre <deywd12@gmail.com>
2026-05-05 12:55:33 -0300

    fix(auth): adicionando @clerk/ui para estabilidade visual

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ClerkProvider } from "@clerk/react";
+import { ui } from "@clerk/ui";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider afterSignOutUrl="/">
+    <ClerkProvider ui={ui} afterSignOutUrl="/">
       <App />
     </ClerkProvider>
   </StrictMode>,
